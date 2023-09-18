742224b6add263810e9a8c47b2728f1b819e70ea
Chloe Page <135153095+ChloeSAPage@users.noreply.github.com>
2023-09-18 21:07:06 +0100

    fixed errors

--- Day 10 - Calculator/main.py
+++ Day 10 - Calculator/main.py
@@ -2,20 +2,19 @@ import os
 
 
 def add(first_num, second_num):
-    result = first_num + second_num
-    return result
+    return first_num + second_num
 
 def subract(first_num, second_num):
-    result = first_num - second_num
-    return result
+    return first_num - second_num
 
-def multiply(first_num, second_num):
-    result = first_num * second_num
-    return result
+def multiply(first_num, second_num): 
+    return first_num * second_num
 
 def divide(first_num, second_num):
-    result = first_num / second_num
-    return result
+    try:
+        return first_num / second_num
+    except ZeroDivisionError:
+        print("You cannot divide a number by 0")
 
 operations = {
     '+': add, 
@@ -24,14 +23,35 @@ operations = {
     '/': divide
               }
 
-first_num = float(input("What is the first number? "))
+while True: # Handle issue
+    try:
+        first_num = float(input("What is the first number? "))
+        break
+    except ValueError:
+        print("Enter a valid number")
+
 while True:
-    operation = input("+\n-\n*\n/\nPick an operator\n")
-    second_num = float(input("What is the second number? "))
+    for operation in operations:
+        print(operation)
+
+    while True:
+        try:
+            operation = input("Pick an operator\n")
+            calc = operations[operation]
+            second_num = float(input("What is the second number? "))
+            break
+        except ValueError:
+            print("Enter a valid number")
+            continue
+        except KeyError:
+            print("Enter a valid operator")
     
-    calc = operations[operation]
     answer = calc(first_num, second_num)
     
+    if answer == None:
+        print(f"First number is {first_num}")
+        continue
+
     print(f"{first_num} {operation} {second_num} = {answer}")
 
     yes_no = input(f"Type 'y' to keep calculating with {answer},'n' for a new calculation or 'e' to exit  " ).lower()
@@ -42,8 +62,13 @@ while True:
         
         case "n":
             os.system('cls')
-            first_num = int(input("What is the first number? "))
-            continue
+            while True:
+                try:
+                    first_num = float(input("What is the first number? "))
+                    break
+                except ValueError:
+                    print("Enter a valid number")
+                    continue
         
         case "y":
             first_num = answer
