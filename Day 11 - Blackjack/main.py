966eb4c3015612f59eeb0236264c8a9c21f578a1
Chloe Page <135153095+ChloeSAPage@users.noreply.github.com>
2023-10-16 17:57:06 +0100

    removed redundant code

--- Day 11 - Blackjack/main.py
+++ Day 11 - Blackjack/main.py
@@ -1,56 +1,37 @@
 import random
 
+
 # Draw Cards
 cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 user_cards = random.choices(cards, k=2)
 dealer_cards =  random.choices(cards, k=2)
 # Display Cards
 print(f"You {user_cards}")
-print(sum(user_cards))
 print(f"Dealers [{dealer_cards[0]}, ?]")
+
 # Game
 while True:
-    answer = input("Do you want another card? Y/N ").lower().strip()
+    answer = input("Draw another card? Y/N ").lower().strip()
 
     if answer == "y":
         user_cards.append(random.choice(cards))
         print(user_cards)
-        print(sum(user_cards))
         pass
-    
-    if sum(user_cards) < 21 and sum(dealer_cards) > 21:
-        print(f"You:{user_cards}")
-        print(f"Dealer:{dealer_cards}")
-        print("Dealer went bust! You win!")
-        break
 
-    elif sum(user_cards) > 21:
+    if sum(user_cards) > 21:
         print(f"You:{user_cards}")
         print(f"Dealer:{dealer_cards}")
         print("You went bust! You lose!")
         break
 
-    elif sum(user_cards) == 21 and not sum(dealer_cards) == 21:
+    elif sum(user_cards) == 21:
         print(f"You:{user_cards}")
         print(f"Dealer:{dealer_cards}")
-        print("You win!")
-        break
-
-    elif sum(dealer_cards) == 21 and not sum(user_cards) == 21:
-        print(f"You:{user_cards}")
-        print(f"Dealer:{dealer_cards}")
-        print("You lose!")
-        break
-    
-    elif sum(user_cards) == 21 and sum(dealer_cards) == 21:
-        print(f"You:{user_cards}")
-        print(f"Dealer:{dealer_cards}")
-        print("You Draw!")
+        print("You got 21! You win!")
         break
     
     if answer == "n":
         while True:
-            print(f"Dealer: {sum(dealer_cards)}")
             if sum(dealer_cards) < 21:
                 if sum(user_cards) > sum(dealer_cards):
                     dealer_cards.append(random.choice(cards))
@@ -72,6 +53,6 @@ while True:
             elif sum(dealer_cards) == 21:
                 print(f"You:{user_cards}")
                 print(f"Dealer:{dealer_cards}")
-                print("You lose!")
+                print("Dealer got 21! You lose!")
                 break
         break
